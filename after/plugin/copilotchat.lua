86f160b0691a95e7f8d672992bd90888a81e0272
Jaden Hums <jadenhums51@gmail.com>
2025-10-01 23:12:10 -0700

    copilot and edits

---
+++ after/plugin/copilotchat.lua
@@ -0,0 +1,11 @@
+local chat = require("CopilotChat")
+
+chat.setup({
+    model = 'gpt-4.1',
+    window = {
+        layout = "vertical",
+        relative = "editor",
+        width = 0.3,
+        border = "single",
+    },
+})
--- lua/jadenhums/lsp.lua
+++ lua/jadenhums/lsp.lua
@@ -3,3 +3,10 @@ vim.lsp.enable({
 	"racketls",
 	"gopls"
 })
+
+-- Autoformat before save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function(ev)
+    vim.lsp.buf.format({ bufnr = ev.buf })
+  end,
+})
