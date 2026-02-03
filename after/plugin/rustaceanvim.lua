43f58f96b3f4c2ce17eb817e2649818bcd6b5e98
Jaden Hums <jadenhums51@gmail.com>
2026-02-03 11:34:23 -0800

    update rust plugins and add 99

--- after/plugin/rustaceanvim.lua
+++
@@ -1,6 +0,0 @@
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.general.positionEncodings = { "utf-16" }
-
-vim.g.rustaceanvim = {
-    capabilities = capabilities,
-}
