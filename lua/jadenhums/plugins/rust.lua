e8ed681f0840d32529e9993fcda93b00f83bf8ba
Jaden Hums <jadenhums51@gmail.com>
2026-02-15 18:08:40 -0800

    remove rustacevim, switching to only use rust-analyzer

--- lua/jadenhums/plugins/rust.lua
+++
@@ -1,9 +0,0 @@
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.general.positionEncodings = { "utf-16" }
-
-return
-{
-    'mrcjkb/rustaceanvim',
-    version = '^7', -- Recommended
-    lazy = false,   -- This plugin is already lazy
-}
